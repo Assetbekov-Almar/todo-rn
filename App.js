9f6dc5635c494e57a72921b88bce74c454e988dd
Almar <almar.asetbekov@gmail.com>
2021-04-25 22:34:54 +0600

    changed file

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
-import React, { useState, useEffect } from "react";
+
+// todo app 
+// Almar Assetbekov 20MD0107
+
+import React, { useState } from "react";
 import {
   StyleSheet,
-  Text,
   TextInput,
   View,
   Button,
@@ -13,27 +16,16 @@ import TodoList from "./src/components/TodoList";
 
 export default function App() {
   const [title, setTitle] = useState("");
-
-  // iniitalize empty object todo
   const [todo, setTodo] = useState({});
-
-  // Initalize empty array to store todos
   const [todos, setTodos] = useState([]);
-
-  // function to add todo object in todo list
   const addTodo = () => {
     if (title.length > 0) {
-      // Add todo to the list
       setTodos([...todos, { key: Date.now(), name: title, isChecked: false }]);
-      // clear the value of the textfield
       setTitle("");
     }
   };
 
-  // function to mark todo as checked or unchecked
   const checkTodo = id => {
-    // loop through todo list and look for the the todo that matches the given id param
-    // update the state using setTodos function
     setTodos(
         todos.map(todo => {
           if (todo.key === id) {
@@ -44,19 +36,12 @@ export default function App() {
     );
   };
 
-  // function to delete todo from the todo list
   const deleteTodo = id => {
-    // loop through todo list and return todos that don't match the id
-    // update the state using setTodos function
     setTodos(todos.filter(todo => {
       return todo.key !== id;
     }));
   };
 
-  useEffect(() => {
-    console.log(todos.length, "TodoList length");
-    //console.log(todos);
-  }, [todos]);
 
   return (
       <View style={styles.container}>
